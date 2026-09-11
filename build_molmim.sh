8a0ad93db34c38a2b81bbb1514e33033daa709ca
Chris Zweck <chris.zweck@reannz.co.nz>
2026-09-11 13:32:29 +1200

    Include instructions for getting container via github

--- build_molmim.sh
+++ build_molmim.sh
@@ -5,6 +5,9 @@ unset APPTAINER_BIND
 APPTAINER_CACHEDIR=$(mktemp -d)
 APPTAINER_TMPDIR=${APPTAINER_CACHEDIR}
 
-#Build container 
+#Build container and make readable to everyone 
 apptainer build --force molmim.sif molmim.def
 chmod 640 molmim.sif
+
+#Should also be available via
+# apptainer pull molmim.sif oras://ghcr.io/acsrc-shoc2/molmim/molmim:latest
